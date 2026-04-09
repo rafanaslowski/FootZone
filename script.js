4aa4380633cda0f6278fc58da22bc3a338f9ea06
Matheus Gomes da Rosa <matheus_g.rosa@hotmail.com>
2026-04-09 19:38:27 -0300

    macara para campos empresa e ususario

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function MascaraNome (input) {
     valor = valor.replace(/[^a-zA-ZÀ-ÿ\s]/g, "");
     input.value = valor;
 
-    if (valor.length > 50) {
-        input.value = valor.slice(0, 50);
+    if (valor.length > 100) {
+        input.value = valor.slice(0, 100);
     }
 }
 // mascara para cahve de acesso, permitindo apenas números e limitando a 8 caracteres
@@ -26,4 +26,55 @@ function mascaraEmail (input) {
 
     valor = valor.replace(/[^a-zA-Z0-9@._-]/g, "");
     input.value = valor.toLowerCase();
+
+    if (valor.length > 100) {
+        input.value = valor.slice(0, 100);
     }
+}
+
+// mascara para cpf, permitindo apenas números e limitando a 11 caracteres
+function mascaraCPF (input) {
+    let valor = input.value;
+
+    valor = valor.replace(/\D/g, "");
+
+     
+    valor = valor.slice(0,11);
+    
+
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    
+    
+    input.value = valor;
+}
+
+// mascara para telefone permitindo apenas números e limitando a 11 caracteres e formatando 
+function mascaraTelefone (input) {
+    let valor = input.value;    
+
+    valor = valor.replace(/\D/g, "");
+    valor = valor.slice(0,11);
+
+    valor = valor.replace(/(\d{2})(\d)/, "($1) $2");
+    valor = valor.replace(/(\d{5})(\d)/, "$1-$2"); 
+    
+    input.value = valor;
+}
+
+// mascara para cnpj, permitindo apenas números e limitando a 14 caracteres e formatando
+function mascaraCNPJ (input) {
+    let valor = input.value;  
+
+    valor = valor.replace(/\D/g, "");
+    valor = valor.slice(0,14); 
+         
+    valor = valor.replace(/(\d{2})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d)/, "$1/$2");
+    valor = valor.replace(/(\d{4})(\d{1,2})$/, "$1-$2");
+
+    input.value = valor;
+}
+
